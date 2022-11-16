8180ad733b338378a543011c365dedd934e21e74
JABV75 <93306875+JABV75@users.noreply.github.com>
2022-11-16 15:14:56 -0600

    Initial Commit

---
+++ RETO_APP/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RETO_APP
+//
+//  Created by José Armando Benvenuto Valerdi  on 15/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func home(_ sender: Any) {
+    }
+    
+    @IBAction func profile(_ sender: Any) {
+    }
+    
+    @IBAction func settings(_ sender: Any) {
+    }
+}
+
---
+++ RETO_APP/ViewController2.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController2.swift
+//  RETO_APP
+//
+//  Created by José Armando Benvenuto Valerdi  on 15/11/22.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func menu(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    /*
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+    }
+    */
+
+}
